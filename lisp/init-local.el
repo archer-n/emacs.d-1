b9d942d248c3d160eb1289072e85be76102e379c
archer <lazyarcher.wang@gmail.com>
2022-03-21 11:50:20 +0800

    Plantuml support

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -230,5 +230,34 @@ For example: ((nil . ((miniprogram-mode . t))))"
 (setq insert-translated-name-translate-engine "youdao")
 (global-set-key (kbd "M-s n") 'insert-translated-name-insert)
 
+
+;;; plantuml
+(require-package 'plantuml-mode)
+(setq plantuml-jar-path (expand-file-name "~/.cache/plantuml/plantuml.jar"))
+(setq plantuml-default-exec-mode 'jar)
+(when (not (file-exists-p plantuml-jar-path))
+  (require 'plantuml-mode)
+  (plantuml-download-jar))
+
+(maybe-require-package 'flycheck-plantuml)
+(flycheck-plantuml-setup)
+
+(defun company-plantuml (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-plantuml))
+    (prefix (let* ((symbol (company-grab-symbol))
+                   (max-match-result (try-completion symbol plantuml-kwdList)))
+              (if (length> max-match-result 0)
+                  symbol)))
+    (candidates (all-completions arg plantuml-kwdList))))
+
+(add-hook 'plantuml-mode-hook
+          (lambda ()
+            (setq-local company-backends '(company-plantuml
+                                           (company-dabbrev-code company-keywords)
+                                           company-dabbrev))))
+
+
 (provide 'init-local)
 ;;; init-local.el ends here
