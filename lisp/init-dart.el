1da11d42c1d37d68b52f6b50b40f13273d6f105d
archer <lazyarcher.wang@gmail.com>
2023-07-07 09:15:13 +0800

    Remove project-try-dart.

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -14,17 +14,8 @@
   (define-key dart-mode-map (kbd "C-c C-o") 'dart-format-buffer))
 
 
-;;; LSP
-(defun project-try-dart (dir)
-  (let ((project (or (locate-dominating-file dir "pubspec.yaml")
-                     (locate-dominating-file dir "BUILD"))))
-    (if project
-        (cons 'dart project)
-      (cons 'transient dir))))
-(add-hook 'project-find-functions #'project-try-dart)
-(cl-defmethod project-roots ((project (head dart)))
-  (list (cdr project)))
 
+;;; LSP
 (add-hook 'dart-mode-hook 'eglot-ensure)
 
 
