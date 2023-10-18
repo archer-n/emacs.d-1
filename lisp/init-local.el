3b098934ff043877c3df13e9b035ad4a12792acd
Wangfan <wangfan00001@gmail.com>
2023-10-18 10:27:37 +0800

    Add translation plugin

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -21,6 +21,78 @@
 (with-eval-after-load 'alert
   (setq-default alert-default-style 'libnotify))
 
+
+;; sdcv
+(require-package 'posframe)
+(require 'sdcv)
+(setq sdcv-say-word-p t)               ;say word after translation
+
+(setq sdcv-dictionary-data-dir (expand-file-name "~/.stardict/dic")) ;setup directory of stardict dictionary
+
+(setq sdcv-dictionary-simple-list    ;setup dictionary list for simple search
+      '("简明英汉字典"
+        "简明汉英字典"
+        "计算机词汇"
+        "牛津现代英汉双解词典"
+        "懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        "KDic11万英汉词典"
+
+        ))
+
+(setq sdcv-dictionary-complete-list     ;setup dictionary list for complete search
+      '("简明英汉字典"
+        "简明汉英字典"
+        "计算机词汇"
+        "牛津现代英汉双解词典"
+        "懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        "KDic11万英汉词典"
+        ))
+
+(require-package 'fanyi)
+;; Emacs 28+
+(setq read-extended-command-predicate #'command-completion-default-include-p)
+
+(defun kimim/sdcv-translate-result-advice (word dictionary-list)
+  (let* ((arguments
+          (cons word
+                (mapcan
+                 (lambda (d) (list "-u" d)) dictionary-list)))
+         (result (mapconcat
+                  (lambda (result)
+                    (let-alist result
+                      (format
+                       "## %s\n%s\n\n" .dict .definition)))
+                  (apply #'sdcv-call-process arguments)
+                  "")))
+    (if (string-empty-p result)
+        sdcv-fail-notify-string
+      result)))
+
+(advice-add 'sdcv-translate-result
+            :override
+            #'kimim/sdcv-translate-result-advice)
+
+(defun kimim/fanyi-dwim-add-sdcv (word)
+  (let ((buf (get-buffer fanyi-buffer-name)))
+    (with-current-buffer buf
+      (let ((inhibit-read-only t)
+            (inhibit-point-motion-hooks t))
+        ;; Clear the previous search result.
+        (point-max)
+        (insert "# SDCV\n\n")
+        (insert
+         (sdcv-search-with-dictionary
+          word sdcv-dictionary-complete-list))
+        (insert "\n\n")
+        (beginning-of-buffer)))))
+
+(advice-add 'fanyi-dwim :after
+            #'kimim/fanyi-dwim-add-sdcv)
+
+(global-set-key (kbd "M-s M-w") 'fanyi-dwim)
+
 (provide 'init-local)
 
 ;;; init-local.el ends here
