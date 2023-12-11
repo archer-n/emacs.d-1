f4cdd32afb89781af442ecac988dfd2794b1508a
Wangfan <wangfan00001@gmail.com>
2023-12-11 09:09:55 +0800

    Tweak.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -14,41 +14,13 @@
 
 
 ;; 设置字体大小
-(set-face-attribute 'default nil :height 110)
+;; (set-face-attribute 'default nil :height 110)
 
 
 ;; alter
 (with-eval-after-load 'alert
   (setq-default alert-default-style 'libnotify))
 
-
-;; sdcv
-(require-package 'posframe)
-(require 'sdcv)
-(setq sdcv-say-word-p t)               ;say word after translation
-
-(setq sdcv-dictionary-data-dir (expand-file-name "~/.stardict/dic")) ;setup directory of stardict dictionary
-
-(setq sdcv-dictionary-simple-list    ;setup dictionary list for simple search
-      '("简明英汉字典"
-        "简明汉英字典"
-        "计算机词汇"
-        "牛津现代英汉双解词典"
-        "懒虫简明英汉词典"
-        "懒虫简明汉英词典"
-        "KDic11万英汉词典"
-
-        ))
-
-(setq sdcv-dictionary-complete-list     ;setup dictionary list for complete search
-      '("简明英汉字典"
-        "简明汉英字典"
-        "计算机词汇"
-        "牛津现代英汉双解词典"
-        "懒虫简明英汉词典"
-        "懒虫简明汉英词典"
-        "KDic11万英汉词典"
-        ))
 
 (provide 'init-local)
 
